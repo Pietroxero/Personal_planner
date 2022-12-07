4f70acb8f8cd4b9a1405e3fd0e18fe669a465cbb
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2022-12-06 17:12:34 -0800

    added in header timer to display current date and time

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -20,4 +20,7 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+    //complete
+    $('#currentDay').text(dayjs().format('MMMM DD, YYYY [at] h:mm:ss a'));
+
   });
