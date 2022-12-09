2fd26f044cd5f532ee269d88ec77d93bb4d9acc1
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2022-12-08 21:35:47 -0800

    added in for loops for the array and chagned data attributes to have the div classes generate to the html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -62,81 +62,175 @@ var dailySchedule = [
 
 ];
 
-var timeDisplayEl = $("#currentDay");
-
-
-$(document).ready(function () 
-{
-loading();
-
+$(document).ready(function () {
 
 function displayDate() {
     var nowTime = dayjs().format("MMMM DD, YYYY [at] h:mm:ss a");
-    timeDisplayEl.text(nowTime);
+    $("#currentDay").text(nowTime);
   }
   displayDate();
   setInterval(displayDate, 1000);
+  
+  function save(event) {
+    let hour = event.target.getAttribute("data-hour");
+    let input = $(`textarea[data-hour="${hour}"]`);
+    console.log(input.val());
+    for ( let i = 0; i <dailySchedule.length; i++){
+        if(dailySchedule[i].tag ==hour){
+            dailySchedule[i].entry = input.val();
+        }
+    }
+    //save to locale storage loop
+  }
+  
+  function createSchedule() {
+    for (let i = 0; i < dailySchedule.length; i++) {
+      // Create the table row
+      let row = $("<div>");
+      row.addClass("row");
+  
+      // create the row elements
+      let div = $("<div>");
+      div.addClass("col-2 col-md-1 hour text-center py-3");
+      div.text(dailySchedule[i].hours);
+  
+      let textbox = $("<textarea>");
+      textbox.attr("data-hour", dailySchedule[i].tag);
+      textbox.addClass("col-8 col-md-10 description");
+      textbox.val(dailySchedule[i].entry);
+  
+      if (dayjs().hour() > dailySchedule[i].tag) {
+        textbox.addClass("past");
+      } else if (dayjs().hour() === dailySchedule[i].tag) {
+        textbox.addClass("present");
+      } else {
+        textbox.addClass("future");
+      }
+  
+      let button = $("<button>");
+      button.addClass("btn col-2 col-md-1 saveBtn");
+      button.attr("aria-label", "save");
+      button.attr("data-hour", dailySchedule[i].tag);
+      button.click(save);
+  
+      let icon = $("<i>");
+      icon.addClass("fas fa-save");
+      icon.attr("data-hour", dailySchedule[i].tag);
+      icon.attr("aria-hidden", "true");
+      button.append(icon);
+  
+      // append elements to the row
+      row.append(div);
+      row.append(textbox);
+      row.append(button);
+  
+      $("#dailyplanner").append(row);
+    }
+  }
+  //make a for loop to loop through the daily array, and checking the tag value = to hour
 
-  var dailyplanner = $('#dailyplanner'); 
-   var nowTime = dayjs().format("H");
+//   function saveEntry (index) 
+// {
+//      var textArea = $("textArea" + index);
+//      if(textArea.val()!=="")
+//  {
+//  dailySchedule[index].entry = textArea.val();
+//  localStorage.setItem("dailySchedule", JSON.stringify("dailySchedule"));
+//  }
+//  else{
+//      alert("no entry to save")
+//  }
+//  };
 
-  $.each(dailySchedule, function(i, time){
+//  saveEntry();
+  
+  createSchedule();
+});
 
-    dailyplanner.append("div id=\"time" + i +"\"" + "class=\"row\"></div>");
-    $("#time"+i).append("<div id=\"currentTime" + i + "\"" + "class=\"col-2 hour\">" + time.hours + "</div>");
-    let Hourplan = thetime.tag;
+
+
+
+
+
+// var timeDisplayEl = $("#currentDay");
+
+
+// $(document).ready(function () 
+// {
+// loading();
+
+
+// function displayDate() {
+//     var nowTime = dayjs().format("MMMM DD, YYYY [at] h:mm:ss a");
+//     timeDisplayEl.text(nowTime);
+//   }
+//   displayDate();
+//   setInterval(displayDate, 1000);
+
+//   var dailyplanner = $('#dailyplanner'); 
+//    var nowTime = dayjs().format("H");
+
+//   $.each(dailySchedule, function(i, time){
+
+//     dailyplanner.append("div id=\"time" + i +"\"" + "class=\"row\"></div>");
+//     $("#time"+i).append("<div id=\"currentTime" + i + "\"" + "class=\"col-2 hour\">" + time.hours + "</div>");
+//     let Hourplan = time.tag;
     
-    var wibblyWobbly = "";
-    if (nowTime > Hourplan){
-        wibblyWobbly = "past";
-    }
-    else if (nowTime == Hourplan){ 
-        wibblyWobbly = "present";
-    }
-    else if (nowTime <  Hourplan){
-        wibblyWobbly = "future";
-    }
+//     var wibblyWobbly = "";
+//     if (nowTime > Hourplan){
+//         wibblyWobbly = "past";
+//     }
+//     else if (nowTime == Hourplan){ 
+//         wibblyWobbly = "present";
+//     }
+//     else if (nowTime < Hourplan) {
+//         wibblyWobbly = "future";
+//     }
     
     //build out and correct for text area. 12/7/2022
-     $("#time"+i).append("<textarea=\"textarea" + i + "\"" + "class=\"col-8" + wibblyWobbly + "\"textarea\">" + dailySchedule[i].entry + "</>");
-    $("#time"+i).append("<div id=\"saveBtn" + i + "data-index=\"" + i + "\"" + "class=\"col-2 saveBtn\">" + "save" + "</div>");
+    //  $("#time"+i).append("<textarea=\"textarea" + i + "\"" + "class=\"col-8" + wibblyWobbly + "\"textarea\">" + dailySchedule[i].entry + "</>");
+    // $("#time"+i).append("<div id=\"saveBtn" + i + "data-index=\"" + i + "\"" + "class=\"col-2 saveBtn\">" + "save" + "</div>");
   
-});
+// });
 
+//idea to build, need event listener for the save btn click event, but also need to
+//build a for loop for the planner
+//create the div class and
+// $("savebtn").click(function(event){
+//     event.preventDefault();
+//     var elements = event.target;
+//     var index = parseInt($(elements).attr("data-index"),10);
+//     console.log(elements);
+//     saveEntry(index);
+//     });
 
-$(".savebtn").click(function(event){
-    var elements = event.target;
-    var index = parseInt($(elements).attr("data-index"),10);
-    saveEntry(index);
-    });
+// });
 
-});
+// function loading() {
+//     var data = localStorage.getItem("dailySchedule");
+//     if (data) {
+//         var dailyArray = JSON.parse(data);
+//         $.each(dailyArray, function(i, item) {
+//             dailySchedule[i].entry = item.entry;
+//         });
+//     }
+//     else {
+//         localStorage.setItem("dailySchedule", JSON.stringify(dailySchedule));
+//     }
+// }
 
-function loading() {
-    var data = localStorage.getItem("dailySchedule");
-    if (data) {
-        var dailyArray = JSON.parse(data);
-        $.each(dailyArray, function(i, item) {
-            dailySchedule[i].entry = item.entry;
-        });
-    }
-    else {
-        localStorage.setItem("dailySchedule", JSON.stringify(dailySchedule));
-    }
-}
-
-function saveEntry (index) 
-{
-    var textArea = $("textField" + index);
-    if(textArea.val()!=="")
-{
-dailySchedule[index].entry = textArea.val();
-localStorage.setItem("dailySchedule", JSON.stringify("dailySchedule"));
-}
-else{
-    alert("no entry to save")
-}
-};
+// function saveEntry (index) 
+// {
+//     var textArea = $("textArea" + index);
+//     if(textArea.val()!=="")
+// {
+// dailySchedule[index].entry = textArea.val();
+// localStorage.setItem("dailySchedule", JSON.stringify("dailySchedule"));
+// }
+// else{
+//     alert("no entry to save")
+// }
+// };
 
   
 
